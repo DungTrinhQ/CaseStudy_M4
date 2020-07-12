16d6da796d5a63a6e5372bf70724bb747dd2e0d1
DungTrinhQ <dungtrinh0702@gmail.com>
2020-07-12 20:00:36 +0700

    create LikeRepository

--- src/main/java/com/codegym/casestudy/repositories/LikeRepository.java
+++ src/main/java/com/codegym/casestudy/repositories/LikeRepository.java
@@ -1,4 +1,9 @@
 package com.codegym.casestudy.repositories;
 
-public class LikeRepository {
+import com.codegym.casestudy.models.Like;
+import com.codegym.casestudy.models.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface LikeRepository extends PagingAndSortingRepository<Like,Long> {
+    Like findLikeByUser(User user);
 }
