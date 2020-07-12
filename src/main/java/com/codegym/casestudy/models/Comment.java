0627a19eb5fa3143ba7ba7d8d365b48e65a6a988
DungTrinhQ <dungtrinh0702@gmail.com>
2020-07-12 19:42:41 +0700

    create Comment model

---
+++ src/main/java/com/codegym/casestudy/models/Comment.java
@@ -0,0 +1,23 @@
+package com.codegym.casestudy.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty
+    private String text;
+
+    @NotNull
+    @ManyToOne
+    private User user;
+}
