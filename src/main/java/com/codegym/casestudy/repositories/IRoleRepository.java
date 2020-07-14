39a3967d4715f6e8b04559a82c604f4f19bc5fdd
DungTrinhQ <dungtrinh0702@gmail.com>
2020-07-14 21:59:30 +0700

    create IRoleRepository

---
+++ src/main/java/com/codegym/casestudy/repositories/IRoleRepository.java
@@ -0,0 +1,8 @@
+package com.codegym.casestudy.repositories;
+
+import com.codegym.casestudy.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IRoleRepository extends CrudRepository<Role, Long> {
+    Role findByPermission(String permission);
+}
