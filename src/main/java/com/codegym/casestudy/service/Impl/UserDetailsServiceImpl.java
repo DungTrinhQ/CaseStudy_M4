0c18e06db32458d1f05e9eee7ca385b20e331d84
DungTrinhQ <dungtrinh0702@gmail.com>
2020-07-14 22:05:34 +0700

    create UserDetailsServiceImpl

---
+++ src/main/java/com/codegym/casestudy/service/Impl/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.codegym.casestudy.service.Impl;
+
+import com.codegym.casestudy.models.Role;
+import com.codegym.casestudy.models.User;
+import com.codegym.casestudy.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        Set<Role> roles = user.getRoles();
+        for (Role role : roles) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getPermission()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), grantedAuthorities);
+    }
+}
