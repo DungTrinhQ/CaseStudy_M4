ae1996619bf9b232325a6d4fa90ebb9d9476e74e
Hieu.Le <letrunghieu2602@gmail.com>
2020-07-15 08:48:24 +0700

    đã thêm chức năng tìm kiếm theo tiêu đề

--- src/main/java/com/codegym/casestudy/repositories/IBlogRepository.java
+++ src/main/java/com/codegym/casestudy/repositories/IBlogRepository.java
@@ -1,12 +1,14 @@
 package com.codegym.casestudy.repositories;
 
 import com.codegym.casestudy.models.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IBlogRepository extends CrudRepository<Blog,Long> {
+public interface IBlogRepository extends JpaRepository<Blog,Long> {
     @Query(value = "SELECT * FROM blog ORDER BY post_time DESC", nativeQuery = true)
     Iterable<Blog> findAllByDateDesc();
+    Iterable<Blog> findAllByTitleContainingOrderByPostTimeDesc(String keyWord);
 }
